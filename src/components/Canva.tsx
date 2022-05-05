cc0b09064208e734923d7cc60fba083118c1eeaf
Guilherme Vier <guilhermevier456@gmail.com>
2022-05-05 09:20:58 -0300

    (change) Desenhando os objetos

--- src/components/Canva.tsx
+++ src/components/Canva.tsx
@@ -1,13 +1,17 @@
 import Sketch from 'react-p5';
 import p5Types from 'p5';
 
+import { ObjectsProviderContext } from './Provider';
+
 export default function Canva() {
+  const { objects } = ObjectsProviderContext();
   const setup = (val: any, parentCanvas: Element) => {
     const p5 = val as p5Types;
     const parent = document.getElementsByClassName('canvaArea')[0];
     p5.createCanvas(parent.clientWidth, parent.clientHeight, p5.WEBGL).parent(
       parentCanvas
     );
+    p5.noFill();
 
     p5.frameRate(60);
   };
@@ -15,12 +19,10 @@ export default function Canva() {
   const draw = (val: any) => {
     const p5 = val as p5Types;
     p5.background(54);
-    p5.strokeWeight(0.5);
-    p5.sphere(100);
-  };
+    p5.strokeWeight(1);
+    p5.orbitControl();
 
-  const mouseDragged = (val: any) => {
-    const p5 = val as p5Types;
+    objects.forEach((object) => object.drawSphere(p5));
   };
 
   const windowResized = (val: any) => {
@@ -29,12 +31,5 @@ export default function Canva() {
     p5.resizeCanvas(parent.clientWidth, parent.clientHeight);
   };
 
-  return (
-    <Sketch
-      setup={setup}
-      draw={draw}
-      windowResized={windowResized}
-      mouseDragged={mouseDragged}
-    />
-  );
+  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
 }
