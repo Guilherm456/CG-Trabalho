de8c2454f8847e5229af05669b3fbef7142649d9
Guilherm456 <guilhermevier456@gmail.com>
2022-07-14 23:23:50 -0300

    (change) Tranposição pela GPU

--- src/utils/shader.tsx
+++ src/utils/shader.tsx
@@ -20,11 +20,11 @@ precision mediump float;
 // Transformation matrices
 uniform mat4 vSRCMatrix;
 uniform mat4 vProjectionMatrix;
+uniform mat4 vViewMatrix;
 
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
-uniform vec2 uScreenSize;
 uniform mat4 uViewMatrix;
 
 attribute vec3 aPosition;
@@ -32,19 +32,18 @@ attribute vec3 aPosition;
 // This is a varying variable, which in shader terms means that it will be passed from the vertex shader to the fragment shader
 varying vec2 vTexCoord;
 
-void main() {
+mat4 transpose(mat4 m) {
+  return mat4(m[0][0], m[1][0], m[2][0], m[3][0],
+              m[0][1], m[1][1], m[2][1], m[3][1],
+              m[0][2], m[1][2], m[2][2], m[3][2],
+              m[0][3], m[1][3], m[2][3], m[3][3]);
+}
 
+void main() {
   
   vec4 normalizedPosition = vec4(aPosition, 1.0);
-  // normalizedPosition = uViewMatrix * normalizedPosition;
-    
-  // doing .xy means we do the same math for both x and y positions
-  // normalizedPosition.x = normalizedPosition.x - uScreenSize[0]/2.0;
-  // normalizedPosition.y = normalizedPosition.y - uScreenSize[1]/2.0;
 
-  // vec4 viewModelPosition = uModelViewMatrix * normalizedPosition;
-  // gl_Position = uProjectionMatrix * viewModelPosition;
-  gl_Position=  vSRCMatrix * vProjectionMatrix * uViewMatrix * normalizedPosition;
+  gl_Position=  transpose(vSRCMatrix) * transpose(vProjectionMatrix) * transpose(vViewMatrix) * normalizedPosition;
   gl_Position.xy /= gl_Position.w;
 }
 `;
