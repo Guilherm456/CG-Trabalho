ab9f8c7da2bd4f2c652dd6e74f2f457cab448f3a
Guilherm456 <guilhermevier456@gmail.com>
2022-07-20 17:57:36 -0300

    (change) Remoção de código

--- src/utils/others.tsx
+++ src/utils/others.tsx
@@ -1,12 +1,5 @@
 import p5Types from 'p5';
 
-export function HexToRGB(hex: string): [number, number, number] {
-  const r = parseInt(hex.substring(0, 2), 16);
-  const g = parseInt(hex.substring(2, 4), 16);
-  const b = parseInt(hex.substring(4, 6), 16);
-  return [r, g, b];
-}
-
 export function getNormal(p5: p5Types, toNormal: number[][]) {
   const P1 = p5.createVector(...toNormal[0]);
   const P2 = p5.createVector(...toNormal[1]);
